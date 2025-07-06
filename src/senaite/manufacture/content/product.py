c28b5f3db0b8ddd7cee54c48fc7afdf50714eb0b
hochkofler <hochkofler94@gmail.com>
2025-07-06 17:53:24 -0400

    add getter and setter for title

--- src/senaite/manufacture/content/product.py
+++ src/senaite/manufacture/content/product.py
@@ -136,18 +136,18 @@ class Product(Item):
             return None
         return schema[fieldname].set
 
-    # @security.protected(permissions.View)
-    # def getTitle(self):
-    #     accessor = self.accessor("title")
-    #     return accessor(self)
+    @security.protected(permissions.View)
+    def getTitle(self):
+        accessor = self.accessor("title")
+        return accessor(self)
 
-    # @security.protected(permissions.ModifyPortalContent)
-    # def setTitle(self, value):
-    #     current = self.title
-    #     value = value.strip()
+    @security.protected(permissions.ModifyPortalContent)
+    def setTitle(self, value):
+        current = self.title
+        value = value.strip()
         
-    #     if current and value != current:
-    #         raise ValueError(_("The product code cannot be changed"))
+        if current and value != current:
+            raise ValueError(_("The product code cannot be changed"))
 
-    #     check_title(value, self)
-    #     self.mutator("title")(self, value)
+        check_title(value, self)
+        self.mutator("title")(self, value)
