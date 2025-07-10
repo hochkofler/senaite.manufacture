001793d0935a28bb5a5a00e65cb67f6e8bcf90bb
hochkofler <hochkofler94@gmail.com>
2025-07-10 15:57:41 -0400

    extend batch content type

--- src/senaite/manufacture/content/fields.py
+++ src/senaite/manufacture/content/fields.py
@@ -1,8 +1,9 @@
 from archetypes.schemaextender.field import ExtensionField
 from bika.lims.browser.fields import UIDReferenceField as UIDATReferenceField
-from senaite.core.schema.uidreferencefield import UIDReferenceField as UIDDXReferenceField
-from senaite.core.schema.datetimefield import DatetimeField
-from senaite.core.schema.fields import IntField
+from senaite.core.browser.fields.datetime import DateTimeField
+from senaite.core.schema import UIDReferenceField as UIDDXReferenceField
+#from senaite.core.schema.fields import IntField
+from Products.Archetypes.Field import IntegerField
 
 class ExtATUIDReferenceField(ExtensionField, UIDATReferenceField):
     """Field Extender of core's UIDReferenceField for AT types
@@ -10,9 +11,9 @@ class ExtATUIDReferenceField(ExtensionField, UIDATReferenceField):
 class ExtUIDReferenceField(ExtensionField, UIDDXReferenceField):
     """Field Extender of core's UIDReferenceField for DX types
     """
-class ExtDateTimeField(ExtensionField, DatetimeField):
+class ExtDateTimeField(ExtensionField, DateTimeField):
     """Field extender of core's DateTimeField
     """
-class ExtIntegerField(ExtensionField, IntField):
+class ExtIntegerField(ExtensionField, IntegerField):
     """Field extender of core's IntegerField
     """
