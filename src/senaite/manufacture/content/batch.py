ccdd50506117ed18f6f2e2e2262b1addbc25aca6
hochkofler <hochkofler94@gmail.com>
2025-07-10 17:08:57 -0400

    fix

--- src/senaite/manufacture/content/batch.py
+++ src/senaite/manufacture/content/batch.py
@@ -1,10 +1,19 @@
 from zope.interface import implements
 from zope.component import adapts
 from archetypes.schemaextender.interfaces import ISchemaExtender
+from archetypes.schemaextender.field import ExtensionField
+from Products.Archetypes.Field import StringField, IntegerField, ReferenceField
 from Products.Archetypes.Widget import StringWidget, IntegerWidget, ReferenceWidget
 from bika.lims.interfaces import IBatch
-from fields import ExtIntegerField
-from senaite.manufacture import messageFactory as _
+
+class StringExtensionField(ExtensionField, StringField):
+    pass
+
+class IntegerExtensionField(ExtensionField, IntegerField):
+    pass
+
+class ReferenceExtensionField(ExtensionField, ReferenceField):
+    pass
 
 class BatchSchemaExtender(object):
     implements(ISchemaExtender)
@@ -15,19 +24,20 @@ class BatchSchemaExtender(object):
 
     def getFields(self):
         return [
-            ExtIntegerField(
-                title=_(
-                    u"title_batch_size",
-                    default=u"Batch Size",
-                ),
+            IntegerExtensionField(
+                name="batchSize",
                 widget=IntegerWidget(
-                    label= _(
-                        u"label_batch_size",
-                        default=u"Batch size"),
-                    description=_(
-                        u"description_batch_size",
-                        default=u"Batch size in units of the product")
+                    label=u"Batch Size",
+                    description=u"Total quantity produced for this batch"
                 ),
-                required=True,
+                required=False,
+            ),
+            IntegerExtensionField(
+                name="releasedQuantity",
+                widget=IntegerWidget(
+                    label=u"Released Quantity",
+                    description=u"Quantity released for distribution"
+                ),
+                required=False,
             ),
         ]
