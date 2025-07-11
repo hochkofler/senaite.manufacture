8f747e278839ad7d70326592a836f97c394dc194
hochkofler <hochkofler94@gmail.com>
2025-07-11 16:42:48 -0400

    change batch according to senaite.patient

--- src/senaite/manufacture/content/batch.py
+++ src/senaite/manufacture/content/batch.py
@@ -1,10 +1,11 @@
-from zope.interface import implements
+from zope.interface import implements, implementer
 from zope.component import adapts
-from archetypes.schemaextender.interfaces import ISchemaExtender
+from archetypes.schemaextender.interfaces import ISchemaExtender, IBrowserLayerAwareExtender, IOrderableSchemaExtender
 from archetypes.schemaextender.field import ExtensionField
 from Products.Archetypes.Field import StringField, IntegerField, ReferenceField
-from Products.Archetypes.Widget import StringWidget, IntegerWidget, ReferenceWidget
+from Products.Archetypes.Widget import IntegerWidget, ReferenceWidget
 from bika.lims.interfaces import IBatch
+from senaite.manufacture.interfaces import ISenaiteManufactureLayer
 
 class StringExtensionField(ExtensionField, StringField):
     pass
@@ -15,29 +16,40 @@ class IntegerExtensionField(ExtensionField, IntegerField):
 class ReferenceExtensionField(ExtensionField, ReferenceField):
     pass
 
-class BatchSchemaExtender(object):
-    implements(ISchemaExtender)
-    adapts(IBatch)
-
-    def __init__(self, context):
-        self.context = context
-
-    def getFields(self):
-        return [
-            IntegerExtensionField(
+BatchSize = IntegerExtensionField(
                 name="batchSize",
                 widget=IntegerWidget(
                     label=u"Batch Size",
                     description=u"Total quantity produced for this batch"
                 ),
                 required=False,
-            ),
-            IntegerExtensionField(
+            )
+
+ReleasedQuantity = IntegerExtensionField(
                 name="releasedQuantity",
                 widget=IntegerWidget(
                     label=u"Released Quantity",
                     description=u"Quantity released for distribution"
                 ),
                 required=False,
-            ),
+            )
+
+
+
+@implementer(IOrderableSchemaExtender, IBrowserLayerAwareExtender)
+class BatchSchemaExtender(object):
+    """Schema extender for the Batch content type."""
+    adapts(IBatch)
+    layer = ISenaiteManufactureLayer
+
+    def __init__(self, context):
+        self.context = context
+
+    def getOrder(self, schematas):
+        return schematas
+    
+    def getFields(self):
+        return [
+            BatchSize,
+            ReleasedQuantity,
         ]
